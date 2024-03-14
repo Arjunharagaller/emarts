961f1eb85c0801ab7c2a036b86b156f97c79cce9
arjun <arjunhm01@gmail.com>
2024-03-14 23:39:03 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux'
 import Store from './Redux/Store';
